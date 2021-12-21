434bf2f9205b900e5687dab9f550a90da2091ebd
ReckerGTX <caiquesenacosta2020@gmail.com>
2021-12-21 18:17:52 -0300

    primeiro commit do projeto

---
+++ Enum/Genero.cs
@@ -0,0 +1,25 @@
+namespace DIO.Series
+{
+	public enum Genero
+	{
+		Acao = 1,
+		Terror = 2,
+
+		Ficcao = 3,
+
+		Drama = 4,
+
+		Comedia = 5,
+
+		Legendado = 6,
+
+		Dublado = 7,
+
+		Nacional = 8,
+
+		Documentario = 9,
+
+		Romance = 10
+
+	}
+}
